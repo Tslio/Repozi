95b2f6b4f9605a282468c695949210d6f6da5568
Tslio <oe_mol@mail.ru>
2025-09-23 23:48:13 +0300

    Задание выполнено

---
+++ Задание-1/Задание-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Иосиф Бродский. Натюрморт");
+            Console.WriteLine("Вещи и люди нас");
+            Console.WriteLine("окружают. И те,");
+            Console.WriteLine("и эти терзают глаз.");
+            Console.WriteLine("Лучше жить в темноте.");
+        }
+    }
+}
