9010fa96e5d8c072b88a4531cac85e02ec83c295
Tslio <oe_mol@mail.ru>
2025-10-08 18:25:37 +0300

    Задание 5 выполнено

---
+++ Задание-5/Задание-5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = Calculate(3, 11) + Calculate(5, 13) + Calculate(7, 17);
+            Console.WriteLine(Math.Round(x, 3));
+        }
+
+        static double Calculate(double a, double b) =>
+            (1 + a * a) / (1 + b * b);
+    }
+}
