fe005e228efb152c5772af3f800eb0511168f643
Tslio <oe_mol@mail.ru>
2025-10-08 14:27:02 +0300

    Задание 4 выполнено

---
+++ Задание-4/Задание-4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число для расчёта по формуле (в градусах)");
+         double x = double.Parse(Console.ReadLine());
+
+            double f = calculation(x);
+            Console.WriteLine("Ответ равен " + f);
+
+        }
+
+        static double calculation(double x)
+        {
+            double xRad = x * Math.PI / 180;
+            double tgX = Math.Tan(xRad);
+            double sinX =Math.Sin(xRad);
+            double cosX = Math.Cos(xRad);
+            double chislitel = tgX + Math.Sqrt(sinX * sinX + 4);
+            double znam = cosX * cosX + 4;
+            return chislitel / znam;
+        }
+    }
+}
