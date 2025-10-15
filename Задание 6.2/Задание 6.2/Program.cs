d57d3eedc554eb032f1d6d26076e0ddfe57626df
Tslio <oe_mol@mail.ru>
2025-10-15 13:04:23 +0300

    Задание 6 выполнено

---
+++ Задание 6.2/Задание 6.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string StartWord = "телега";
+            Console.WriteLine(StartWord.Remove(0, 2)
+                + StartWord.Remove(1, 5));
+            Console.WriteLine(StartWord.Remove(0, 5)
+                + StartWord.Remove(1, 5)
+                + StartWord.Remove(4, 2).Remove(0, 2)
+                + StartWord.Remove(1,5));
+        }
+    }
+}
---
+++ Задание 6.2/Задание 6.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Задание 6.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Задание 6.2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("401d5242-d76b-4948-a72b-5d9383da4ad9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
