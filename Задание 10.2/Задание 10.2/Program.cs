2df174ad6996b873b3611411c64ad8571b0d4a7a
Tslio <oe_mol@mail.ru>
2025-11-19 12:29:05 +0300

    Задание выполнено

---
+++ Задание 10.2/Задание 10.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_10._2
+{
+    internal class Program
+    {
+        static void Main()
+        { Console.WriteLine("Введите количество административных единиц p: ");
+            int p = int.Parse(Console.ReadLine());
+
+            int totalPopulation = 0;
+            int totalArea = 0;
+
+            for (int i = 1; i <= p; i++)
+            {
+                Console.WriteLine("  Введите количество жителей (в тыс. чел.): ");
+                Console.WriteLine("  Введите количество жителей (в тыс. чел.): ");
+                int population = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("  Введите площадь (км²): ");
+                int area = int.Parse(Console.ReadLine());
+
+                totalPopulation += population;
+                totalArea += area;
+            }
+
+            double density = totalPopulation * 1000 / totalArea;
+
+            Console.WriteLine("Средняя плотность населения: ", density, "чел./км²");
+        }
+    }
+}
---
+++ Задание 10.4/Задание 10.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_10._4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите натуральное число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int minDigit = 9;    
+            int minPosition = 1;
+            int position = 1;    
+
+            for (int numberForCalculation = number; numberForCalculation > 0; numberForCalculation /= 10, position++)
+            {
+                int digit = numberForCalculation % 10; 
+
+                if (digit < minDigit)
+                {
+                    minDigit = digit;
+                    minPosition = position;
+                }
+            }
+
+            Console.WriteLine("Порядковый номер минимальной цифры справа налево: " + minPosition);
+        }
+    }
+}
