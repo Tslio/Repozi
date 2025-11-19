d64c84ee63e4c9b13a2e3b24cd2d0b669ef5a2de
Tslio <oe_mol@mail.ru>
2025-11-19 12:48:54 +0300

    Задание 10 выполнено

---
+++ Задание10.2/Задание10.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание10._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите количество административных единиц: ");
+            int unit = int.Parse(Console.ReadLine());
+
+            int countryPopulation = 0;
+            int countryArea = 0;
+
+            for (int i = 1; i <= unit; i++)
+            {
+                Console.WriteLine("Административная единица номер " + i);
+                Console.WriteLine("Введите количество жителей (в тыс. чел.): ");
+                int population = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("  Введите площадь (кв. км.): ");
+                int area = int.Parse(Console.ReadLine());
+
+                countryPopulation += population;
+                countryArea += area;
+            }
+
+            double density = countryPopulation * 1000 / countryArea;
+            Console.WriteLine("Средняя плотность населения: " + density + " чел./кв. км.");
+        }
+    }
+}
---
+++ Задание10.6/Задание10.6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание10._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <= 10000; number++)
+            {
+                int sum = 0;
+                for (int i = 1; i < number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+
+                if (sum == number)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+        }
+    }
+}
