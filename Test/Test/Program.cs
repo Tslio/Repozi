2aa9b4267875a1da0a84d7be2d8f8d6ccd6fbc29
Tslio <oe_mol@mail.ru>
2026-01-14 08:42:40 +0300

    Задание выполнено

---
+++ Test/Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for (int i = 1; i <= 1_000_000; i++)
+            {
+                if (IsHarshad(i))
+                    count++;
+            }
+
+            Console.WriteLine(count);
+        }
+        static bool IsHarshad(int number)
+        {
+            int sumOfDigits = 0;
+            int digit = number;
+
+            while (digit > 0)
+            {
+                sumOfDigits += digit % 10;
+                digit /= 10;
+            }
+
+            return number % sumOfDigits == 0;
+        }
+    }
+}
