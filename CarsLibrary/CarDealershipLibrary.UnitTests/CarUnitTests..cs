f60635b29d0ec9c7bf66b7ce8e49431f1f5a0fbb
Tslio <oe_mol@mail.ru>
2026-03-29 22:32:57 +0500

    задание сделано

---
+++ CarsLibrary/CarDealershipLibrary.UnitTests/CarUnitTests..cs
@@ -0,0 +1,50 @@
+﻿using CarsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CarsLibrary.UnitTests
+{
+    [TestFixture]
+    public class CarUnitTests
+    {
+        [Test]
+        public void ConstructorTest()
+        {
+            var car = CreateTestCar();
+            Assert.That(car.Brand, Is.EqualTo("Toyota"));
+            Assert.That(car.Year, Is.EqualTo(2020));
+            Assert.That(car.VIN, Is.EqualTo("JT123456789012345"));
+            Assert.That(car.BodyType, Is.EqualTo(CarBodyType.Sedan));
+            Assert.That(car.Price, Is.EqualTo(1500000m));
+            Assert.That(car.SaleDate.ToShortDateString(), Is.EqualTo("20.03.2026"));
+            Assert.That(car.BuyerFullName, Is.EqualTo("Dmitriy Ivanov"));
+        }
+
+        [Test]
+        public void GetInfoTest()
+        {
+            var car = CreateTestCar();
+            var info = car.GetInfo();
+            Assert.That(info.Length, Is.EqualTo(2));
+            Assert.That(info[0], Is.EqualTo("Toyota (2020) - VIN: JT123456789012345"));
+            Assert.That(info[1], Is.EqualTo($"Тип кузова: Sedan. Цена: {1500000:C}. Дата продажи: 20.03.2026. Покупатель: Dmitriy Ivanov."));
+        }
+
+        private Car CreateTestCar()
+        {
+            return new Car(
+                "Toyota",
+                2020,
+                "JT123456789012345",
+                CarBodyType.Sedan,
+                1500000m,
+                "20.03.2026",
+                "Dmitriy Ivanov"
+            );
+        }
+    }
+}
---
+++ CarsLibrary/CarsLibrary/Car.cs
@@ -0,0 +1,40 @@
+﻿using CarsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsLibrary
+{
+    public class Car
+    {
+        public string Brand {get; set;}
+        public int Year {get; set;}
+        public readonly string VIN;
+        public CarBodyType BodyType { get; set;}
+        public decimal Price {get; set;}
+        public DateTime SaleDate {get; set;}
+        public string BuyerFullName {get; set;}
+
+        public Car(string brand, int year, string vin, CarBodyType bodyType, decimal price, string saleDate, string buyerFullName)
+        {
+            Brand = brand;
+            Year = year;
+            VIN = vin;
+            BodyType = bodyType;
+            Price = price;
+            BuyerFullName = buyerFullName;
+            if (!DateTime.TryParse(saleDate, out DateTime parsedDate))
+                throw new ArgumentException("Неверный формат даты продажи");
+            SaleDate = parsedDate;
+        }
+        public virtual string[] GetInfo()
+        {
+            var info = new string[2];
+            info[0] = $"{Brand} ({Year}) - VIN: {VIN}";
+            info[1] = $"Тип кузова: {BodyType}. Цена: {Price:C}. Дата продажи: {SaleDate:d}. Покупатель: {BuyerFullName}.";
+            return info;
+        }
+    }
+}
