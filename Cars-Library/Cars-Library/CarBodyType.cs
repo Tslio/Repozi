c5843c48488d8839b043d8281299237d5998ba80
Tslio <oe_mol@mail.ru>
2026-03-29 23:31:05 +0500

    Сделано

---
+++ Cars-Library/Cars-Library/CarBodyType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars_Library
+{
+    public enum CarBodyType
+    {
+        Sedan,
+        Hatchback,
+        Wagon,
+        Cabriolet
+    }
+}
